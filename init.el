242755991c9312ddd3518d435df85dafe5a81c0a
George Malamidis <thr4sh@gmail.com>
2022-07-17 11:17:31 +0100

    Added melpa, go and rust setup

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
 (savehist-mode 1)
 (put 'erase-buffer 'disabled nil)
 (setq make-backup-files nil)
@@ -5,6 +11,11 @@
 (setq-default indent-tabs-mode nil)
 (setq show-paren-delay 0)
 (show-paren-mode 1)
+
+(setq gofmt-command "/usr/local/go/bin/gofmt")
+(add-hook 'before-save-hook #'gofmt-before-save)
+(setq rust-rustfmt-bin "~/.cargo/bin/rustfmt")
+(setq rust-format-on-save t)
 
 (defun readlink-f ()
   "show current buffer's full path"
