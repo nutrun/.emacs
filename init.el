9822104c9619118e12d486386cdd0c8e3c8c00d6
George Malamidis <thr4sh@gmail.com>
2022-07-17 11:36:28 +0100

    Installed go-mode and rust-mode, adding elpa dir

--- init.el
+++ init.el
@@ -21,3 +21,15 @@
   "show current buffer's full path"
   (interactive)
   (message (buffer-file-name (current-buffer))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(rust-mode go-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
